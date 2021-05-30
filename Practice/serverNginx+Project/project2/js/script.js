03f0b2e3dbf89d567c10c1f9fc35ecf400c67c03
europaplus <basshuntep@mail.ru>
2021-05-30 17:51:26 +0300

    added dockerNginx

--- Practice/serverNginx+Project/project2/js/script.js
+++ Practice/serverNginx+Project/project2/js/script.js
@@ -186,4 +186,57 @@ document.addEventListener('DOMContentLoaded', () => {
         'происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и' +
         'импортных вегетарианских стейков. ',
         430/27, '.menu .container', 'menu__item').render();
+
+    // Forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с Вами свяжемся',
+        failure: 'Что-то пошло не так...',
+    }
+
+    forms.forEach(item => {
+        postData(item);
+    })
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            const fromData = new FormData(form);
+            request.setRequestHeader('Content-type', 'application/json');
+            const object = {};
+            fromData.forEach(function (value, key) {
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
+            request.send(json);
+            request.addEventListener('load', ()=> {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        })
+    }
+
+    function showThanksModal() {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.classList.add('hide');
+    }
 });
