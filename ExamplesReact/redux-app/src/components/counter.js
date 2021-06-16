171798e2ab8a736e34040c0645b1a3078ff7aea3
europaplus <basshuntep@mail.ru>
2021-06-16 18:01:21 +0300

    GOT v2

---
+++ ExamplesReact/redux-app/src/components/counter.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {connect} from "react-redux";
+import * as actions from '../actions';
+import {bindActionCreators} from "redux";
+
+const Counter = ({counter, inc, dec, res}) => {
+    return (
+        <div className="content">
+            <div className="counter-wrap">
+                <h1 id="counter">{counter}</h1>
+            </div>
+            <div className="btn-block">
+                <button onClick={inc} className="btn btn-inc">INC</button>
+                <button onClick={dec} className="btn btn-dec">DEC</button>
+                <button onClick={res} className="btn btn-res">RES</button>
+            </div>
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        counter: state,
+    }
+}
+
+export default connect(mapStateToProps, actions)(Counter);
---
+++ ExamplesReact/redux-app/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import {createStore} from "redux";
+import reducer from "./reducer";
+import App from "./components/app";
+import {Provider} from "react-redux";
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    , document.getElementById('root'));
